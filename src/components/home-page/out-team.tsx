c59c1e00c26bebe194014d874dfa97f49c5eb355
Sampath_Kumara <84168369+sampath-kumaramd@users.noreply.github.com>
2025-01-06 21:29:18 +0530

    complete home page

--- src/components/home-page/out-team.tsx
+++ src/components/home-page/out-team.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { Card, CardContent, CardHeader } from '../ui/card';
+import { TitleText } from '../common/title-text';
 
 export default function OutTeam() {
   const getborderClass = (index: number) => {
@@ -22,7 +23,7 @@ export default function OutTeam() {
   return (
     <div className="bg-black text-white py-16 container mx-auto">
       <div className="text-center space-y-4">
-        <h2 className="text-4xl font-bold">OUR TEAM</h2>
+        <TitleText>OUR TEAM</TitleText>
         <p className="text-gray-300 max-w-2xl mx-auto">
           Our dedicated team brings together a wealth of knowledge and practical
           insights from both the academic and professional spheres
@@ -34,24 +35,7 @@ export default function OutTeam() {
           {teamMembers.map((member, index) => (
             <div
               key={member.name}
-              className={`text-center space-y-3 rounded-lg p-4
-                 const getborderClass = (index: number) => {
-    if (index === 0) return 'bottom-right-blue-border';
-    if (index === 1) return 'bottom-right-green-border';
-    if (index === 2) return 'bottom-right-yellow-border';
-    if (index === 3) return 'bottom-right-purple-border';
-    return '';
-  };
-
-  const getGradientColor = (index: number) => {
-    // rgb(16, 13, 40)
-    if (index === 0) return 'rgba(16, 13, 40, )'; // dark blue
-    if (index === 1) return 'rgba(13, 42, 21, 0.8)'; // dark green
-    if (index === 2) return 'rgba(46, 28, 16, 0.8)'; // dark purple/navy
-    if (index === 3) return 'rgba(47, 17, 37, 0.8)'; // dark burgundy
-    return 'rgba(228, 226, 255, 0.1)';
-  };
-               `}
+              className={`text-center space-y-3 rounded-lg p-4 ${getborderClass(index)}`}
             >
               <div className="relative overflow-hidden">
                 <div
